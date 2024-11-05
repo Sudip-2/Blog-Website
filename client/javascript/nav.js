08076a272f6c78b179c3ccf9d57d413c0e8b3709
Sudip Paul <p452570@gmail.com>
2024-11-05 18:42:46 +0530

    search mode and light dark mode completed

--- client/javascript/nav.js
+++ client/javascript/nav.js
@@ -18,9 +18,78 @@ searchlogo.addEventListener('click',() => {
     searchbarinput.classList.toggle('active')
 })
 
+// Light dark mode 
+let githublogo = document.querySelector('#github-logo')
+    let githublogoW = "../images/social icons/light/github-mark.png"
+    let githublogoD = "../images/social icons/dark/github-mark-white.png"
+    let currentSrc = githublogo.src.split('/').pop();
+window.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('darkMode') === 'active') {
+        darkmode();
+    } else {
+        lightmode();
+    }
+});
+let ldBtn = document.getElementById('lightdarktogglebtn')
+function lightmode(){
+    document.body.classList.remove('darkmode')
+    document.body.classList.remove('darkmodemain')
+    localStorage.setItem('darkMode','not active')
+    githublogo.src = githublogoW;
+}
+function darkmode(){
+    document.body.classList.add('darkmode')
+    document.body.classList.add('darkmodemain')
+    localStorage.setItem('darkMode','active')
+    githublogo.src = githublogoD;
+}
+ldBtn.addEventListener('click', () => {
+    if(localStorage.getItem('darkMode') == 'not active'){
+        darkmode()
+    }
+    else{
+        lightmode()
+    }
+})
+
 let hamburgerLogo = document.querySelector('.spanForNav')
 let navlinks = document.querySelector('.navHyperLinks')
 
 hamburgerLogo.addEventListener('click',() => {
     navlinks.classList.toggle('active')
 })
+
+// search recomendation
+
+let suggestionBox = document.querySelector('.searchRecomendationBox')
+
+searchbarinput.addEventListener('keyup', async() => {
+    let keywordOfBlogs = await fetch('http://localhost:3000/blogs/title')
+    let availKeywords = await keywordOfBlogs.json()
+    let titles = availKeywords.map(item => item.title);
+    let result = []
+    let input = searchbarinput.value
+    if(input.length){
+        result = titles.filter((keyword) => {
+            return keyword.toLowerCase().includes(input.toLowerCase())
+        })
+    }
+    displaySearchTerm(result)
+    if(!input.length){
+        suggestionBox.innerHTML = " "
+    }
+})
+
+function displaySearchTerm(result) {
+ let content = result.map((item) => {
+    return "<li onclick=selectInput(this) >"+ item +"</li>"
+ })
+
+suggestionBox.innerHTML = "<ul>"+content.join(" ")+"</ul>"
+
+}
+
+function selectInput(list){
+    searchbarinput.value = list.innerHTML
+    suggestionBox.innerHTML = " "
+}
