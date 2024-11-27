7a753750b41bf3d68c83817e5a2bf0f9934c5e7e
Sudip Paul <p452570@gmail.com>
2024-11-28 03:34:41 +0530

    added test cases for contact info through email

--- client/javascript/contactinfo.js
+++ client/javascript/contactinfo.js
@@ -6,6 +6,7 @@ submitbtn.addEventListener('click', async () => {
     let fullName = document.getElementById('nameOfnameEmail')
     let email = document.getElementById('emailOfnameEmail')
     let message = document.getElementById('contactFormMessage')
+    if(email.value != '' && fullName.value != '' && message.value != '' && email.value.includes('@')){
         let senderProfile = {
             name:fullName.value,
             email:email.value,
@@ -26,4 +27,26 @@ submitbtn.addEventListener('click', async () => {
                 popup.close()
             },1500)
         }
+    }
+    if(email.value === '' || !email.value.includes('@') ){
+        popupHeading.textContent = "Enter Valid email"
+            popup.showModal()
+            setTimeout(()=>{
+                popup.close()
+            },1500)
+    }
+    if(fullName.value === ''){
+        popupHeading.textContent = "Enter Name"
+            popup.showModal()
+            setTimeout(()=>{
+                popup.close()
+            },1500)
+    }
+    if(message.value === ''){
+        popupHeading.textContent = "Enter Message"
+            popup.showModal()
+            setTimeout(()=>{
+                popup.close()
+            },1500)
+    }
 })
